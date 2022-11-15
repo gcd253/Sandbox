7e0b3ff861d06fdbf1e186dc2b9af92261aeafc3
Griffin Dooley <dooleygriffin@gmail.com>
2022-11-15 13:09:54 -0500

    Caesar cipher

--- sandbox.js
+++ sandbox.js
@@ -19,3 +19,66 @@
 
 // reverseArrayInPlace([6,5,4,3,2,1,0])
 
+function caesarCipher(s, k) {
+  // Write your code here
+  while (k > 26) {
+    k -= 26;
+  }
+
+  let alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  let capitalsString = alphabetString.toUpperCase();
+  let splitString = s.split("");
+
+  let alphabet = alphabetString.split("");
+  let indexArray = [];
+
+  splitString.map((letter) => {
+    if (alphabet.includes(letter)) {
+      indexArray.push(alphabet.indexOf(letter));
+    } else {
+      indexArray.push(letter);
+    }
+  });
+
+  let portion = alphabet.splice(0, k);
+
+  for (let i = 0; i < portion.length; i++) {
+    alphabet.push(portion[i]);
+  }
+
+  let partialResult = [];
+
+  indexArray.map((number) => {
+    if (typeof number === "number") {
+      partialResult.push(alphabet.join("").charAt(number));
+    } else {
+      partialResult.push(number);
+    }
+  });
+
+  let capitalsMod = alphabet.join("").toUpperCase().split("");
+
+  let result = [];
+
+  partialResult.map((item) => {
+    if (capitalsMod.includes(item)) {
+      result.push(capitalsString.indexOf(item));
+    } else {
+      result.push(item);
+    }
+  });
+
+  let final = [];
+
+  result.map((char) => {
+    if (typeof char === "number") {
+      final.push(capitalsMod.join("").charAt(char));
+    } else {
+      final.push(char);
+    }
+  });
+
+  return final.join("");
+}
+
+caesarCipher("www.abc.xy", 86);
