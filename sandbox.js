22ca5350ddebb7d6d622f9caeb06197c7d86d39d
Griffin Dooley <dooleygriffin@gmail.com>
2022-11-14 19:15:47 -0500

    init

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+// function reverseArray(arr) {
+//   const newArray = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     newArray.unshift(arr[i]);
+//   }
+//   console.log(newArray);
+// }
+
+// reverseArray([3, 2, 1]);
+
+// function reverseArrayInPlace(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let element = arr.pop()
+//     arr.splice(i, 0, element)
+//   }
+//   console.log(arr)
+// }
+
+// reverseArrayInPlace([6,5,4,3,2,1,0])
+
